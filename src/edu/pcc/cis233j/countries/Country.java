ab32d46996c290f846fd3df0dbead3031347f1ff
SaraSquibb18 <42517024+SaraSquibb18@users.noreply.github.com>
2018-10-24 06:22:32 -0700

    Update Country.java

--- src/edu/pcc/cis233j/countries/Country.java
+++ src/edu/pcc/cis233j/countries/Country.java
@@ -10,29 +10,4 @@ public class Country {
 	private long population;
 	private double medianAge;
 	
-	/**
-	 * Create a Country object with the given properties
-	 */
-	public Country(int id, String name, long population, double medianAge) {
-		this.id = id;
-		this.name = name;
-		this.population = population;
-		this.medianAge = medianAge;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public long getPopulation() {
-		return population;
-	}
-
-	public double getMedianAge() {
-		return medianAge;
-	}
-}
+erased.....................................................
